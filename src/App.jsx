ef8f5b00314d444f3aed6c9566f90562ae52c548
Chatpong Deepet <chatpond@outlook.com>
2024-12-02 13:15:30 +0700

    import fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 import { Routes, Route } from "react-router-dom";
-import Add from "./pages/Add";
+import Add from "./pages/Add.jsx";
 import List from "./pages/List";
 import Orders from "./pages/Orders";
 import Login from "./components/Login";
